78e3f6c2bf5fd9b8ec822c6addbd367fa54f5aa1
s1223 <fianjulio@gmail.com>
2019-08-02 17:51:33 +0700

    Add files via upload

--- Object.cpp
+++ Object.cpp
@@ -3,8 +3,10 @@
 #include "Segitiga.h"
 #include <iostream>
 #include <stdio.h>
-#include <glew.h>
-#include <glut.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include"Box2D/Box2D.h"
+#include<stdbool.h>
 const double phi = 3.141592653589793;
 using namespace std;
 float TX[4];
@@ -83,6 +85,138 @@ void save(int jenis, int x, int y) {
 	counter_data += 1;
 }
 
+float box2d_x(float posisi_saat_ini, float batas_bawah) {
+	// Define the gravity vector.
+	b2Vec2 gravity(0.0f, -9.8f);
+
+	// Construct a world object, which will hold and simulate the rigid bodies.
+	b2World world(gravity);
+
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+	// Define the ground box shape.
+	b2PolygonShape groundBox;
+
+	// The extents are the half-widths of the box.
+	groundBox.SetAsBox(50.0f, 10.0f);
+
+	// Add the ground fixture to the ground body.
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	// Define the dynamic body. We set its position and call the body factory.
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0f, posisi_saat_ini);
+	b2Body* body = world.CreateBody(&bodyDef);
+
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, batas_bawah);
+
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+
+	// Set the box density to be non-zero, so it will be dynamic.
+	fixtureDef.density = 1.0f;
+
+	// Override the default friction.
+	fixtureDef.friction = 0.3f;
+
+	// Add the shape to the body.
+	body->CreateFixture(&fixtureDef);
+
+	// Prepare for simulation. Typically we use a time step of 1/60 of a
+	// second (60Hz) and 10 iterations. This provides a high quality simulation
+	// in most game scenarios.
+	float32 timeStep = 1.0f / 60.0f;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	// Instruct the world to perform a single step of simulation.
+		// It is generally best to keep the time step and iterations fixed.
+	world.Step(timeStep, velocityIterations, positionIterations);
+
+	// Now print the position and angle of the body.
+	b2Vec2 position = body->GetPosition();
+	float32 angle = body->GetAngle();
+
+	return position.x;
+}
+
+float box2d_y(float posisi_saat_ini, float batas_bawah) {
+	// Define the gravity vector.
+	b2Vec2 gravity(0.0f, -9.8f);
+
+	// Construct a world object, which will hold and simulate the rigid bodies.
+	b2World world(gravity);
+
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+	// Define the ground box shape.
+	b2PolygonShape groundBox;
+
+	// The extents are the half-widths of the box.
+	groundBox.SetAsBox(50.0f, 10.0f);
+
+	// Add the ground fixture to the ground body.
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	// Define the dynamic body. We set its position and call the body factory.
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0f, posisi_saat_ini);
+	b2Body* body = world.CreateBody(&bodyDef);
+
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, batas_bawah);
+
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+
+	// Set the box density to be non-zero, so it will be dynamic.
+	fixtureDef.density = 1.0f;
+
+	// Override the default friction.
+	fixtureDef.friction = 0.3f;
+
+	// Add the shape to the body.
+	body->CreateFixture(&fixtureDef);
+
+	// Prepare for simulation. Typically we use a time step of 1/60 of a
+	// second (60Hz) and 10 iterations. This provides a high quality simulation
+	// in most game scenarios.
+	float32 timeStep = 1.0f / 60.0f;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	// Instruct the world to perform a single step of simulation.
+		// It is generally best to keep the time step and iterations fixed.
+	world.Step(timeStep, velocityIterations, positionIterations);
+
+	// Now print the position and angle of the body.
+	b2Vec2 position = body->GetPosition();
+	float32 angle = body->GetAngle();
+
+	return position.y;
+}
+
 void onDisplay() {
 	Kotak object1;
 	Segitiga objeknya;
@@ -103,6 +237,9 @@ void onDisplay() {
 		{
 			obj1.gambar_L(dt[i][0], dt[i][1]);
 		}
+		//start Box2D
+		dt[i][1] = box2d_y(dt[i][1],1);
+		//end Box2D
 	}
 
 	//objeknya.segi3();
@@ -115,14 +252,26 @@ void myKeyboard(unsigned char key, int x, int y) {
 	if (key == 'a' || key == 'A') {
 		save(1, x, 600 - y);
 		onDisplay();
+		for (size_t i = 0; i < 100; i++)
+		{
+			onDisplay();
+		}
 	}
 	else if (key == 's' || key == 'S') {
 		save(2, x, 600 - y);
 		onDisplay();
+		for (size_t i = 0; i < 100; i++)
+		{
+			onDisplay();
+		}
 	}
 	else if (key == 'd' || key == 'D') {
 		save(3, x, 600 - y);
 		onDisplay();
+		for (size_t i = 0; i < 100; i++)
+		{
+			onDisplay();
+		}
 	}
 }
 
@@ -158,6 +307,8 @@ void myMouse(int button, int state, int x, int y) {
 }
 
 int main(int argc, char* argv[]) {
+	B2_NOT_USED(argc);
+	B2_NOT_USED(argv);
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
