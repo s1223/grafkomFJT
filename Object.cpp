1a18f0f2c54c35335941e67c5ca9fb4705939921
s1223 <fianjulio@gmail.com>
2019-08-04 18:13:47 +0700

    Add files via upload

--- Object.cpp
+++ Object.cpp
@@ -5,10 +5,8 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GL/glut.h>
-#include "imageloader.h"
 #include"Box2D/Box2D.h"
-//#include<stdbool.h>
-
+#include<stdbool.h>
 const double phi = 3.141592653589793;
 using namespace std;
 float TX[4];
@@ -17,24 +15,6 @@ int counter = 0;
 
 int dt[100][3];
 int counter_data = 0;
-
-GLuint _textureK;
-
-
-GLuint loadTexture(Image* image) {
-	GLuint textureId;
-	glGenTextures(1, &textureId);
-	glBindTexture(GL_TEXTURE_2D, textureId);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width, image->height, 0, GL_RGB, GL_UNSIGNED_BYTE, image->pixels);
-	return textureId;
-}
-
-void initRendering() {
-	Image* image = loadBMP("fti.bmp");
-	_textureK = loadTexture(image);
-	
-	//delete image;
-}
 
 
 Kotak::Kotak() {
@@ -89,24 +69,13 @@ void Segitiga::segi3(float ax, float by) {
 }
 
 void Kotak::gambar_k(float x, float y) {
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, _textureK);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
 	glBegin(GL_LINE_LOOP);
-	glColor3f(1, 1, 1);
-	glTexCoord2f(0.0f, 0.0f);
+	glColor3f(0, 1, 0);
 	glVertex2f(x - 50, y - 50);
-	glTexCoord2f(0.0f, 1.0f);
 	glVertex2f(x - 50, y + 50);
-	glTexCoord2f(1.0f, 1.0f);
 	glVertex2f(x + 50, y + 50);
-	glTexCoord2f(1.0f, 0.0f);
 	glVertex2f(x + 50, y - 50);
 	glEnd();
-	
-	glDisable(GL_TEXTURE_2D);
 }
 
 void save(int jenis, int x, int y) {
@@ -116,71 +85,6 @@ void save(int jenis, int x, int y) {
 	counter_data += 1;
 }
 
-float box2d_x(float posisi_saat_ini, float batas_bawah) {
-	// Define the gravity vector.
-	b2Vec2 gravity(0.0f, -9.8f);
-
-	// Construct a world object, which will hold and simulate the rigid bodies.
-	b2World world(gravity);
-
-	// Define the ground body.
-	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(0.0f, -10.0f);
-
-	// Call the body factory which allocates memory for the ground body
-	// from a pool and creates the ground box shape (also from a pool).
-	// The body is also added to the world.
-	b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
-	// Define the ground box shape.
-	b2PolygonShape groundBox;
-
-	// The extents are the half-widths of the box.
-	groundBox.SetAsBox(50.0f, 10.0f);
-
-	// Add the ground fixture to the ground body.
-	groundBody->CreateFixture(&groundBox, 0.0f);
-
-	// Define the dynamic body. We set its position and call the body factory.
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, posisi_saat_ini);
-	b2Body* body = world.CreateBody(&bodyDef);
-
-	// Define another box shape for our dynamic body.
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(1.0f, batas_bawah);
-
-	// Define the dynamic body fixture.
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
-
-	// Set the box density to be non-zero, so it will be dynamic.
-	fixtureDef.density = 1.0f;
-
-	// Override the default friction.
-	fixtureDef.friction = 0.3f;
-
-	// Add the shape to the body.
-	body->CreateFixture(&fixtureDef);
-
-	// Prepare for simulation. Typically we use a time step of 1/60 of a
-	// second (60Hz) and 10 iterations. This provides a high quality simulation
-	// in most game scenarios.
-	float32 timeStep = 1.0f / 60.0f;
-	int32 velocityIterations = 6;
-	int32 positionIterations = 2;
-
-	// Instruct the world to perform a single step of simulation.
-		// It is generally best to keep the time step and iterations fixed.
-	world.Step(timeStep, velocityIterations, positionIterations);
-
-	// Now print the position and angle of the body.
-	b2Vec2 position = body->GetPosition();
-	float32 angle = body->GetAngle();
-
-	return position.x;
-}
 
 float box2d_y(float posisi_saat_ini, float batas_bawah) {
 	// Define the gravity vector.
@@ -238,24 +142,91 @@ float box2d_y(float posisi_saat_ini, float batas_bawah) {
 	int32 positionIterations = 2;
 
 	// Instruct the world to perform a single step of simulation.
-		// It is generally best to keep the time step and iterations fixed.
+	// It is generally best to keep the time step and iterations fixed.
 	world.Step(timeStep, velocityIterations, positionIterations);
-
+	
 	// Now print the position and angle of the body.
 	b2Vec2 position = body->GetPosition();
 	float32 angle = body->GetAngle();
-
 	return position.y;
 }
 
 void onDisplay() {
-	initRendering();
-	
 	Kotak object1;
 	Segitiga objeknya;
 	Lingkaran obj1;
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+
+	b2Vec2 gravity(0.0f, -9.8f);
+	b2World myWorld =  b2World(gravity);
+
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = myWorld.CreateBody(&groundBodyDef);
+
+	// Define the ground box shape.
+	b2PolygonShape groundBox;
+
+	// The extents are the half-widths of the box.
+	groundBox.SetAsBox(50.0f, 10.0f);
+
+	// Add the ground fixture to the ground body.
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	// Define the dynamic body. We set its position and call the body factory.
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0f, 40.0f);
+	b2Body* body = myWorld.CreateBody(&bodyDef);
+
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, 1.0f);
+
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+
+	// Set the box density to be non-zero, so it will be dynamic.
+	fixtureDef.density = 1.0f;
+
+	// Override the default friction.
+	fixtureDef.friction = 0.3f;
+
+	// Add the shape to the body.
+	body->CreateFixture(&fixtureDef);
+
+	float32 timeStep = 1.0f / 60.f;
+	int32 velocityIterations = 10;
+	int32 positionIterations = 8;
+	myWorld.Step(timeStep, velocityIterations, positionIterations);
+	b2Vec2 position = body->GetPosition();
+	float32 angle = body->GetAngle();
+	printf("%4.5f %4.5f %4.5f\n", position.x, position.y, angle);
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
 	for (int i = 0; i < 100; i++)
 	{
 		if (dt[i][2] == 1)
@@ -278,17 +249,19 @@ void onDisplay() {
 	//objeknya.segi3();
 
 
+
 	glutSwapBuffers();
+
 }
 
 void myKeyboard(unsigned char key, int x, int y) {
 	if (key == 'a' || key == 'A') {
 		save(1, x, 600 - y);
 		onDisplay();
-		for (size_t i = 0; i < 100; i++)
+		/*for (size_t i = 0; i < 100; i++)
 		{
 			onDisplay();
-		}
+		}*/
 	}
 	else if (key == 's' || key == 'S') {
 		save(2, x, 600 - y);
@@ -354,7 +327,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	glMatrixMode(GL_PROJECTION);
-	//glLoadIdentity();
+	glLoadIdentity();
 	gluOrtho2D(0, 800, 0, 600);
 
 	glutMouseFunc(myMouse);
