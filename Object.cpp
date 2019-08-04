41e0a97c4aed8fc190b8bd2f99868c9784f7be1a
s1223 <fianjulio@gmail.com>
2019-08-04 20:58:29 +0700

    Add files via upload

--- Object.cpp
+++ Object.cpp
@@ -69,7 +69,7 @@ void Segitiga::segi3(float ax, float by) {
 }
 
 void Kotak::gambar_k(float x, float y) {
-	glBegin(GL_POLYGON);
+	glBegin(GL_LINE_LOOP);
 	glColor3f(0, 1, 0);
 	glVertex2f(x - 50, y - 50);
 	glVertex2f(x - 50, y + 50);
